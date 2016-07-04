0f7cb1f0aefaf1f770c4aca07f4820091f962417
Vinnie Falco <vinnie.falco@gmail.com>
2016-07-04 13:24:30 -0400

    [FOLD]

--- test/core/zlib/deflate.cpp
+++ test/core/zlib/deflate.cpp
@@ -67,12 +67,12 @@ const char deflate_copyright[] =
 /* ===========================================================================
  *  Function prototypes.
  */
-typedef enum {
+enum block_state {
     need_more,      /* block not completed, need more input or more output */
     block_done,     /* block flush performed */
     finish_started, /* finish started, need only more output at next deflate */
     finish_done     /* finish done, accept no more input or output */
-} block_state;
+};
 
 typedef block_state (*compress_func) OF((deflate_state *s, int flush));
 /* Compression function. Returns the block state after the call. */
