030d85628f9eeb0d6f6ea16db0af2467dcea1cc5
Vinnie Falco <vinnie.falco@gmail.com>
2016-08-11 08:45:16 -0400

    [FOLD] split basic_ sources

---
+++ include/beast/detail/zlib/basic_deflate_stream.hpp
@@ -0,0 +1,378 @@
+//
+// Copyright (c) 2013-2016 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// This is a derivative work based on Zlib, copyright below:
+/*
+    Copyright (C) 1995-2013 Jean-loup Gailly and Mark Adler
+
+    This software is provided 'as-is', without any express or implied
+    warranty.  In no event will the authors be held liable for any damages
+    arising from the use of this software.
+
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+       claim that you wrote the original software. If you use this software
+       in a product, an acknowledgment in the product documentation would be
+       appreciated but is not required.
+    2. Altered source versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.
+    3. This notice may not be removed or altered from any source distribution.
+
+    Jean-loup Gailly        Mark Adler
+    jloup@gzip.org          madler@alumni.caltech.edu
+
+    The data format used by the zlib library is described by RFCs (Request for
+    Comments) 1950 to 1952 in the files http://tools.ietf.org/html/rfc1950
+    (zlib format), rfc1951 (deflate format) and rfc1952 (gzip format).
+*/
+
+#ifndef BEAST_ZLIB_BASIC_DEFLATE_STREAM_HPP
+#define BEAST_ZLIB_BASIC_DEFLATE_STREAM_HPP
+
+#include <beast/detail/zlib/zlib.hpp>
+#include <beast/detail/zlib/detail/deflate.hpp>
+#include <cstdlib>
+
+#include <memory>
+
+namespace beast {
+namespace zlib {
+
+/** Raw deflate compressor.
+
+    This is a port of zlib's "deflate" functionality to C++.
+*/
+template<class Allocator>
+class basic_deflate_stream : public z_stream
+{
+    // maximum heap size
+    static std::uint16_t constexpr HEAP_SIZE = 2 * limits::lCodes + 1;
+
+    // size of bit buffer in bi_buf
+    static std::uint8_t constexpr Buf_size = 16;
+
+    // VFALCO This might not be needed, e.g. for zip/gzip
+    enum StreamStatus
+    {
+        EXTRA_STATE = 69,
+        NAME_STATE = 73,
+        COMMENT_STATE = 91,
+        HCRC_STATE = 103,
+        BUSY_STATE = 113,
+        FINISH_STATE = 666
+    };
+
+    struct tree_desc
+    {
+        detail::ct_data *dyn_tree;           /* the dynamic tree */
+        int     max_code;            /* largest code with non zero frequency */
+        detail::static_tree_desc const* stat_desc; /* the corresponding static tree */
+    };
+
+    /* A std::uint16_t is an index in the character window. We use short instead of int to
+     * save space in the various tables. IPos is used only for parameter passing.
+     */
+    using IPos = unsigned;
+
+    enum block_state
+    {
+        need_more,      /* block not completed, need more input or more output */
+        block_done,     /* block flush performed */
+        finish_started, /* finish started, need only more output at next deflate */
+        finish_done     /* finish done, accept no more input or output */
+    };
+
+public:
+    basic_deflate_stream();
+
+    ~basic_deflate_stream();
+
+    int deflate(int flush);
+
+    int deflateSetDictionary(const Byte *dictionary, uInt  dictLength);
+
+    static int deflateEnd (basic_deflate_stream* strm);
+    static int deflateResetKeep (basic_deflate_stream* strm);
+    static int deflateReset (basic_deflate_stream* strm);
+    static int deflateParams (basic_deflate_stream* strm, int level, int strategy);
+    static int deflateTune (basic_deflate_stream* strm,
+        int good_length, int max_lazy, int nice_length, int max_chain);
+    static std::size_t deflateBound (basic_deflate_stream* strm, std::size_t sourceLen);
+    static int deflatePending (basic_deflate_stream* strm, unsigned *pending, int *bits);
+    static int deflatePrime (basic_deflate_stream* strm, int bits, int value);
+    static int deflateInit (basic_deflate_stream* strm, int level);
+    static int deflateInit2 (basic_deflate_stream* strm, int level,
+        int windowBits, int memLevel, int strategy);
+
+private:
+    detail::deflate_tables const& lut_;
+
+    int   status_;        /* as the name implies */
+    Byte *pending_buf_;  /* output still pending */
+    std::uint32_t   pending_buf_size_; /* size of pending_buf */
+    Byte *pending_out_;  /* next pending byte to output to the stream */
+    uInt   pending_;      /* nb of bytes in the pending buffer */
+    int   last_flush_;    /* value of flush param for previous deflate call */
+
+                /* used by deflate.c: */
+
+    uInt  w_size_;        /* LZ77 window size (32K by default) */
+    uInt  w_bits_;        /* log2(w_size)  (8..16) */
+    uInt  w_mask_;        /* w_size - 1 */
+
+    /* Sliding window. Input bytes are read into the second half of the window,
+     * and move to the first half later to keep a dictionary of at least wSize
+     * bytes. With this organization, matches are limited to a distance of
+     * wSize-limits::maxMatch bytes, but this ensures that IO is always
+     * performed with a length multiple of the block size. Also, it limits
+     * the window size to 64K.
+     * To do: use the user input buffer as sliding window.
+     */
+    Byte *window_ = nullptr;
+
+    /* Actual size of window: 2*wSize, except when the user input buffer
+     * is directly used as sliding window.
+     */
+    std::uint32_t window_size_;
+
+    /* Link to older string with same hash index. To limit the size of this
+     * array to 64K, this link is maintained only for the last 32K strings.
+     * An index in this array is thus a window index modulo 32K.
+     */
+    std::uint16_t *prev_;
+
+    std::uint16_t *head_; /* Heads of the hash chains or 0. */
+
+    uInt  ins_h_;          /* hash index of string to be inserted */
+    uInt  hash_size_;      /* number of elements in hash table */
+    uInt  hash_bits_;      /* log2(hash_size) */
+    uInt  hash_mask_;      /* hash_size-1 */
+
+    /* Number of bits by which ins_h must be shifted at each input
+     * step. It must be such that after limits::minMatch steps, the oldest
+     * byte no longer takes part in the hash key, that is:
+     *   hash_shift * limits::minMatch >= hash_bits
+     */
+    uInt  hash_shift_;
+
+    long block_start_;
+    /* Window position at the beginning of the current output block. Gets
+     * negative when the window is moved backwards.
+     */
+
+    uInt match_length_;           /* length of best match */
+    IPos prev_match_;             /* previous match */
+    int match_available_;         /* set if previous match exists */
+    uInt strstart_;               /* start of string to insert */
+    uInt match_start_;            /* start of matching string */
+    uInt lookahead_;              /* number of valid bytes ahead in window */
+
+    uInt prev_length_;
+    /* Length of the best match at previous step. Matches not greater than this
+     * are discarded. This is used in the lazy match evaluation.
+     */
+
+    uInt max_chain_length_;
+    /* To speed up deflation, hash chains are never searched beyond this
+     * length.  A higher limit improves compression ratio but degrades the
+     * speed.
+     */
+
+    uInt max_lazy_match_;
+    /* Attempt to find a better match only when the current match is strictly
+     * smaller than this value. This mechanism is used only for compression
+     * levels >= 4.
+     */
+    /* OR Insert new strings in the hash table only if the match length is not
+     * greater than this length. This saves time but degrades compression.
+     * used only for compression levels <= 3.
+     */
+
+    int level_;    /* compression level (1..9) */
+    int strategy_; /* favor or force Huffman coding*/
+
+    uInt good_match_;
+    /* Use a faster search when the previous match is longer than this */
+
+    int nice_match_; /* Stop searching when current match exceeds this */
+
+                /* used by trees.c: */
+    /* Didn't use detail::ct_data typedef below to suppress compiler warning */
+    detail::ct_data dyn_ltree_[HEAP_SIZE];   /* literal and length tree */
+    detail::ct_data dyn_dtree_[2*limits::dCodes+1]; /* distance tree */
+    detail::ct_data bl_tree_[2*limits::blCodes+1];  /* Huffman tree for bit lengths */
+
+    tree_desc l_desc_;               /* desc. for literal tree */
+    tree_desc d_desc_;               /* desc. for distance tree */
+    tree_desc bl_desc_;              /* desc. for bit length tree */
+
+    std::uint16_t bl_count_[limits::maxBits+1];
+    /* number of codes at each bit length for an optimal tree */
+
+    int heap_[2*limits::lCodes+1];      /* heap used to build the Huffman trees */
+    int heap_len_;               /* number of elements in the heap */
+    int heap_max_;               /* element of largest frequency */
+    /* The sons of heap[n] are heap[2*n] and heap[2*n+1]. heap[0] is not used.
+     * The same heap array is used to build all trees.
+     */
+
+    std::uint8_t depth_[2*limits::lCodes+1];
+    /* Depth of each subtree used as tie breaker for trees of equal frequency
+     */
+
+    std::uint8_t *l_buf_;          /* buffer for literals or lengths */
+
+    /* Size of match buffer for literals/lengths.  There are 4 reasons for
+     * limiting lit_bufsize to 64K:
+     *   - frequencies can be kept in 16 bit counters
+     *   - if compression is not successful for the first block, all input
+     *     data is still in the window so we can still emit a stored block even
+     *     when input comes from standard input.  (This can also be done for
+     *     all blocks if lit_bufsize is not greater than 32K.)
+     *   - if compression is not successful for a file smaller than 64K, we can
+     *     even emit a stored file instead of a stored block (saving 5 bytes).
+     *     This is applicable only for zip (not gzip or zlib).
+     *   - creating new Huffman trees less frequently may not provide fast
+     *     adaptation to changes in the input data statistics. (Take for
+     *     example a binary file with poorly compressible code followed by
+     *     a highly compressible string table.) Smaller buffer sizes give
+     *     fast adaptation but have of course the overhead of transmitting
+     *     trees more frequently.
+     *   - I can't count above 4
+     */
+    uInt  lit_bufsize_;
+
+    uInt last_lit_;      /* running index in l_buf */
+
+    std::uint16_t *d_buf_;
+    /* Buffer for distances. To simplify the code, d_buf and l_buf have
+     * the same number of elements. To use different lengths, an extra flag
+     * array would be necessary.
+     */
+
+    std::uint32_t opt_len_;        /* bit length of current block with optimal trees */
+    std::uint32_t static_len_;     /* bit length of current block with static trees */
+    uInt matches_;       /* number of string matches in current block */
+    uInt insert_;        /* bytes at end of window left to insert */
+
+    /* Output buffer. bits are inserted starting at the bottom (least
+     * significant bits).
+     */
+    std::uint16_t bi_buf_;
+
+    /* Number of valid bits in bi_buf.  All bits above the last valid bit
+     * are always zero.
+     */
+    int bi_valid_;
+
+    /* High water mark offset in window for initialized bytes -- bytes above
+     * this are set to zero in order to avoid memory check warnings when
+     * longest match routines access bytes past the input.  This is then
+     * updated to the new high water mark.
+     */
+    std::uint32_t high_water_;
+
+    static void fill_window(basic_deflate_stream *s);
+    static block_state deflate_stored(basic_deflate_stream *s, int flush);
+    static block_state deflate_fast(basic_deflate_stream *s, int flush);
+    static block_state deflate_slow(basic_deflate_stream *s, int flush);
+    static block_state deflate_rle(basic_deflate_stream *s, int flush);
+    static block_state deflate_huff(basic_deflate_stream *s, int flush);
+
+    static void lm_init        (basic_deflate_stream *s);
+    static void flush_pending  (basic_deflate_stream* strm);
+    static int read_buf        (basic_deflate_stream* strm, Byte *buf, unsigned size);
+    static uInt longest_match  (basic_deflate_stream *s, IPos cur_match);
+
+    static void init_block     (basic_deflate_stream *s);
+    static void pqdownheap     (basic_deflate_stream *s, detail::ct_data *tree, int k);
+    static void gen_bitlen     (basic_deflate_stream *s, tree_desc *desc);
+    static void build_tree     (basic_deflate_stream *s, tree_desc *desc);
+    static void scan_tree      (basic_deflate_stream *s, detail::ct_data *tree, int max_code);
+    static void send_tree      (basic_deflate_stream *s, detail::ct_data *tree, int max_code);
+    static int  build_bl_tree  (basic_deflate_stream *s);
+    static void send_all_trees (basic_deflate_stream *s, int lcodes, int dcodes,
+                                int blcodes);
+
+    static void _tr_init (basic_deflate_stream *s);
+    static int _tr_tally (basic_deflate_stream *s, unsigned dist, unsigned lc);
+    static void _tr_flush_block (basic_deflate_stream *s, char *buf,
+                            std::uint32_t stored_len, int last);
+    static void _tr_flush_bits (basic_deflate_stream *s);
+    static void _tr_align (basic_deflate_stream *s);
+    static void _tr_stored_block (basic_deflate_stream *s, char *bu,
+                            std::uint32_t stored_len, int last);
+
+    static void compress_block (basic_deflate_stream *s, const detail::ct_data *ltree,
+                                const detail::ct_data *dtree);
+    static int  detect_data_type (basic_deflate_stream *s);
+    static void bi_flush       (basic_deflate_stream *s);
+    static void bi_windup      (basic_deflate_stream *s);
+    static void copy_block     (basic_deflate_stream *s, char *buf, unsigned len,
+                                  int header);
+
+    using compress_func = block_state(*)(basic_deflate_stream*, int flush);
+
+    /* Values for max_lazy_match, good_match and max_chain_length, depending on
+     * the desired pack level (0..9). The values given below have been tuned to
+     * exclude worst case performance for pathological files. Better values may be
+     * found for specific files.
+     */
+    struct config {
+       std::uint16_t good_length; /* reduce lazy search above this match length */
+       std::uint16_t max_lazy;    /* do not perform lazy search above this match length */
+       std::uint16_t nice_length; /* quit search above this match length */
+       std::uint16_t max_chain;
+       compress_func func;
+
+       config(
+               std::uint16_t good_length_,
+               std::uint16_t max_lazy_,
+               std::uint16_t nice_length_,
+               std::uint16_t max_chain_,
+               compress_func func_)
+           : good_length(good_length_)
+           , max_lazy(max_lazy_)
+           , nice_length(nice_length_)
+           , max_chain(max_chain_)
+           , func(func_)
+       {
+       }
+    };
+
+    static
+    config
+    get_config(std::size_t level)
+    {
+        switch(level)
+        {
+        //      good lazy nice chain 
+        case 0: return {  0,   0,   0,    0, &deflate_stored}; // store only 
+        case 1: return {  4,   4,   8,    4, &deflate_fast};   // max speed, no lazy matches
+        case 2: return {  4,   5,  16,    8, &deflate_fast};
+        case 3: return {  4,   6,  32,   32, &deflate_fast};
+        case 4: return {  4,   4,  16,   16, &deflate_slow};   // lazy matches
+        case 5: return {  8,  16,  32,   32, &deflate_slow};
+        case 6: return {  8,  16, 128,  128, &deflate_slow};
+        case 7: return {  8,  32, 128,  256, &deflate_slow};
+        case 8: return { 32, 128, 258, 1024, &deflate_slow};
+        default:
+        case 9: return { 32, 258, 258, 4096, &deflate_slow};    // max compression
+        }
+    }
+};
+
+using deflate_stream = basic_deflate_stream<std::allocator<std::uint8_t>>;
+
+} // zlib
+} // beast
+
+#include <beast/detail/zlib/impl/basic_deflate_stream.ipp>
+
+#endif
--- include/beast/detail/zlib/inflate_stream.hpp
+++ include/beast/detail/zlib/inflate_stream.hpp
@@ -35,169 +35,17 @@
 #ifndef BEAST_ZLIB_INFLATE_STREAM_HPP
 #define BEAST_ZLIB_INFLATE_STREAM_HPP
 
-#include <beast/detail/zlib/zlib.hpp>
-#include <beast/detail/zlib/detail/inflate_tables.hpp>
-#include <beast/core/error.hpp>
+#include <beast/detail/zlib/basic_inflate_stream.hpp>
 #include <cstdint>
-#include <cstdlib>
 #include <memory>
 
 namespace beast {
 namespace zlib {
-
-/** Raw deflate decompressor.
-
-    This is a port of zlib's "inflate" functionality to C++.
-*/
-template<class Allocator>
-class basic_inflate_stream : public z_stream
-{
-public:
-    struct params
-    {
-        void const* next_in;        // next input byte
-        std::size_t avail_in;       // number of bytes available at next_in
-        std::size_t total_in = 0;   // total number of input bytes read so far
-
-        void*       next_out;       // next output byte should be put there
-        std::size_t avail_out;      // remaining free space at next_out
-        std::size_t total_out = 0;  // total number of bytes output so far
-    };
-
-    /** Construct a raw deflate decompression stream.
-
-        The window size is set to the default of 15 bits.
-    */
-    basic_inflate_stream();
-
-    /// Destructor.
-    ~basic_inflate_stream();
-
-    /** Reset the stream.
-
-        This puts the stream in a newly constructed state with the
-        specified window size, but without de-allocating any dynamically
-        created structures.
-    */
-    void
-    reset(std::uint8_t windowBits);
-
-#if 0
-    /** Write compressed data to the stream.
-
-        @return `true` if the end of stream is reached.
-    */
-    bool
-    write(params& ps, error_code& ec);
-#endif
-
-    int
-    write(int flush);
-
-private:
-    enum inflate_mode
-    {
-        HEAD,       // i: waiting for magic header
-        FLAGS,      // i: waiting for method and flags (gzip)
-        TIME,       // i: waiting for modification time (gzip)
-        OS,         // i: waiting for extra flags and operating system (gzip)
-        EXLEN,      // i: waiting for extra length (gzip)
-        EXTRA,      // i: waiting for extra bytes (gzip)
-        NAME,       // i: waiting for end of file name (gzip)
-        COMMENT,    // i: waiting for end of comment (gzip)
-        HCRC,       // i: waiting for header crc (gzip)
-        TYPE,       // i: waiting for type bits, including last-flag bit
-        TYPEDO,     // i: same, but skip check to exit inflate on new block
-        STORED,     // i: waiting for stored size (length and complement)
-        COPY_,      // i/o: same as COPY below, but only first time in
-        COPY,       // i/o: waiting for input or output to copy stored block
-        TABLE,      // i: waiting for dynamic block table lengths
-        LENLENS,    // i: waiting for code length code lengths
-        CODELENS,   // i: waiting for length/lit and distance code lengths
-            LEN_,   // i: same as LEN below, but only first time in
-            LEN,    // i: waiting for length/lit/eob code
-            LENEXT, // i: waiting for length extra bits
-            DIST,   // i: waiting for distance code
-            DISTEXT,// i: waiting for distance extra bits
-            MATCH,  // o: waiting for output space to copy string
-            LIT,    // o: waiting for output space to write literal
-        CHECK,      // i: waiting for 32-bit check value
-        LENGTH,     // i: waiting for 32-bit length (gzip)
-        DONE,       // finished check, done -- remain here until reset
-        BAD,        // got a data error -- remain here until reset
-        MEM,        // got an inflate() memory error -- remain here until reset
-        SYNC        // looking for synchronization bytes to restart inflate()
-    };
-
-    inflate_mode mode_;             // current inflate mode
-    int last_;                      // true if processing last block
-    unsigned dmax_;                 // zlib header max distance (INFLATE_STRICT)
-    // VFALCO This might be unused
-    unsigned long total_;           // protected copy of output count
-
-    // sliding window
-    unsigned wbits_;                // log base 2 of requested window size
-    unsigned wsize_;                // window size or zero if not using window
-    unsigned whave_;                // valid bytes in the window
-    unsigned wnext_;                // window write index
-    unsigned char *window_ =
-        nullptr;                    // allocated sliding window, if needed
-
-    // bit accumulator
-    unsigned long hold_;            // input bit accumulator
-    unsigned bits_;                 // number of bits in "in"
-
-    // for string and stored block copying
-    unsigned length_;               // literal or length of data to copy
-    unsigned offset_;               // distance back to copy string from
-
-    // for table and code decoding
-    unsigned extra_;                // extra bits needed
-    
-    // fixed and dynamic code tables
-    detail::code const *lencode_;   // starting table for length/literal codes
-    detail::code const *distcode_;  // starting table for distance codes
-    unsigned lenbits_;              // index bits for lencode
-    unsigned distbits_;             // index bits for distcode
-
-    // dynamic table building
-    unsigned ncode_;                // number of code length code lengths
-    unsigned nlen_;                 // number of length code lengths
-    unsigned ndist_;                // number of distance code lengths
-    unsigned have_;                 // number of code lengths in lens[]
-    detail::code *next_;            // next available space in codes[]
-    unsigned short lens_[320];      // temporary storage for code lengths
-    unsigned short work_[288];      // work area for code table building
-    detail::code codes_[detail::ENOUGH];     // space for code tables
-    int sane_;                      // if false, allow invalid distance too far
-    int back_;                      // bits back of last unprocessed length/lit
-    unsigned was_;                  // initial length of match
-
-private:
-    static
-    void
-    inflate_fast(basic_inflate_stream* strm, unsigned start);
-
-    static
-    int
-    write(basic_inflate_stream* strm, int flush);
-
-    void
-    resetKeep();
-
-    void
-    fixedTables();
-
-    int
-    updatewindow(const Byte *end, unsigned copy);
-};
 
 using inflate_stream =
     basic_inflate_stream<std::allocator<std::uint8_t>>;
 
 } // zlib
 } // beast
-
-#include <beast/detail/zlib/impl/basic_inflate_stream.ipp>
 
 #endif
